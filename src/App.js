45bea515e2541e89a329e78f41f07d28122f8a38
LoiolaBr17 <mateustaua16@gmail.com>
2022-03-01 15:32:48 -0300

    paginacao funcionando 1v

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect,useState} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+
+    const [itens, setItens] = useState([])
+    const [itemPerPage, setItensPerPage] = useState(10)
+    const [currentPage, setCurrentPage] = useState(0)
+
+    const pages = Math.ceil(itens.length / itemPerPage)
+    const startIndex = currentPage * itemPerPage;
+    const endIndex = startIndex + itemPerPage;
+    const currentItens = itens.slice(startIndex, endIndex)
+
+    useEffect(() => {
+      const fetchData = async () => {
+        const result = await fetch('https://jsonplaceholder.typicode.com/todos')
+        .then(response => response.json())
+        .then(data => data)
+
+        setItens(result)
+      }
+      fetchData()
+    }, [])
+
+    return (
+        <div className="App">
+
+          {currentItens.map(item => {
+            return <div className='item'><span>{item.id}</span><span>{item.title}</span><span>{item.completed}</span></div>
+          })}
+
+          <div>
+            {Array.from(Array(pages), (item, index) => {
+              return <button value={index} onClick={(e) => setCurrentPage(Number(e.target.value))}>{index}</button>
+            })}
+          </div>
+
+
+        </div>
+    );
 }
 
 export default App;
